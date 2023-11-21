e065e2e41c1b53ca413cacd71636b3abd0ed4e1c
harry38444 <harry38444@gmail.com>
2023-11-21 10:21:19 -0500

    A
    A

--- vite-project/js/vite.js
+++ vite-project/js/vite.js
@@ -1,43 +1,9 @@
-const DOMSelectors = {
-
-    button: document.getElementById("button"),
-    input: document.querySelector(`#input`),
-    input2: document.querySelector(`#input2`),
-    delete: document.querySelector(`#delete`),
-    form: document.querySelector("#form"),
-    input3: document.querySelector(`#input3`),
-        h2s: document.querySelectorAll("h2"),
-        h3s: document.querySelectorAll("h3")
-    };
-    
-    
-    DOMSelectors.form.addEventListener("submit", function(event){
-       event.preventDefault();
-       console.log(DOMSelectors.CardName.value);
-       DOMSelectors.h2s.forEach(
-        (el)=> el.textContent = (DOMSelectors.CardName.value),
-       )});
-       const card = DOMSelectors.button.addEventListener("click", function(){
-        let input= DOMSelectors.input.value;
-        let input2= DOMSelectors.input2.value;
-        let input3= DOMSelectors.input3.value;
-        DOMSelectors.form.insertAdjacentHTML("beforeend",
-        `<div class="column">
-        <div class="card">
-            <h2 class="card-header" id="column">${input2}</h2>
-    
-            <h3 class="card-info" id="column">${input}</h3>
-    
-            <img src = "${input3}" alt = "" class = "img">
-            <button type="click" class="delete">Delete</button>
-        </div>
-    `);
-       DOMSelectors.input.value = "";
-       DOMSelectors.input2.value ="";
-       DOMSelectors.input3.value ="";
-       document.querySelectorAll('.delete').forEach((button) => {
-        button.addEventListener('click', function(event) {
-            event.target.parentElement.parentElement.remove();
-            });
-        });
-    });
+document.querySelector(".btn").addEventListener("click", function () {
+    if (document.body.classList.contains("light")) {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    }
+  });
