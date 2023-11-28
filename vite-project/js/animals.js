5d982b11827d77ab20a230dc6a7fe8296b1c7101
harry38444 <harry38444@gmail.com>
2023-11-28 10:21:00 -0500

    animals
    new

--- vite-project/js/animals.js
+++ vite-project/js/animals.js
@@ -24,45 +24,45 @@
         price: '1000$'
     },
     {
-        name: 'Dog',
+        name: 'Goat',
         type: 'Herbivore',
-        img: 'https://bousefurhouse.com/wp-content/uploads/revslider/hospital/slider-dog-small.png',
+        img: 'https://static.vecteezy.com/system/resources/previews/024/722/378/original/goat-with-ai-generated-free-png.png',
         price: '1000$'
     },
     {
-        name: 'Dog',
+        name: 'Rabbit',
         type: 'Herbivore',
-        img: 'https://bousefurhouse.com/wp-content/uploads/revslider/hospital/slider-dog-small.png',
+        img: 'https://www.freeiconspng.com/thumbs/rabbit-png/white-sweet-rabbit-png-7.png',
         price: '1000$'
     },
     {
-        name: 'Dog',
+        name: 'Deer',
         type: 'Herbivore',
-        img: 'https://bousefurhouse.com/wp-content/uploads/revslider/hospital/slider-dog-small.png',
+        img: 'https://png.pngtree.com/png-clipart/20230506/original/pngtree-isolated-deer-on-transparent-png-image_9145489.png',
         price: '1000$'
     },
     {
-        name: 'Dog',
+        name: 'Cow',
         type: 'Herbivore',
-        img: 'https://bousefurhouse.com/wp-content/uploads/revslider/hospital/slider-dog-small.png',
+        img: 'https://pngimg.com/d/cow_PNG50570.png',
         price: '1000$'
     },
     {
-        name: 'Dog',
+        name: 'Children',
         type: 'Omnivore',
-        img: 'https://bousefurhouse.com/wp-content/uploads/revslider/hospital/slider-dog-small.png',
+        img: 'https://purepng.com/public/uploads/large/purepng.com-childrenchildrenkidshuman-childchildhappy-kidsklds-1421526966147jzchz.png',
+        price: '100000$'
+    },
+    {
+        name: 'Bear',
+        type: 'Omnivore',
+        img: 'https://png.pngtree.com/png-vector/20230318/ourmid/pngtree-brown-bear-wild-animal-transparent-on-white-background-png-image_6655679.png',
         price: '1000$'
     },
     {
-        name: 'Dog',
+        name: 'Turtle',
         type: 'Omnivore',
-        img: 'https://bousefurhouse.com/wp-content/uploads/revslider/hospital/slider-dog-small.png',
-        price: '1000$'
-    },
-    {
-        name: 'Dog',
-        type: 'Omnivore',
-        img: 'https://bousefurhouse.com/wp-content/uploads/revslider/hospital/slider-dog-small.png',
+        img: 'https://i.pinimg.com/originals/07/7d/51/077d519a8c253bf4dc45048f358b70d9.png',
         price: '1000$'
     },
     {
